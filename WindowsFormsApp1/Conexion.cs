9ae40dbf157b44cb5d83daa28cc825c61ca3a7c2
Estudiante <60031750@idexperujapon.edu.pe>
2025-12-01 15:44:11 -0500

    Conexion

---
+++ WindowsFormsApp1/Conexion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class Conexion
+    {
+       SqlConnection connection = new SqlConnection(" Database=inventario_de_equipos informaticos;Data Source=localhost;UserId=sa;Password=Hyp3r10nPr0_;TrustServerCertificate=True");
+        public SqlConnection AbrirConexion()
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                    MessageBox.Show("Conexión abierta correctamente.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir la conexión: " + ex.Message);
+            }
+            return connection;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cerrar la conexión: " + ex.Message);
+            }
+            return connection;
+        }
+    }
+}
